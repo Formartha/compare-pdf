009190e7915fea464c8270d21012410988984357
Mor Dabastany <morpci@gmail.com>
2024-03-28 22:16:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-# setup.py
 from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='compare_pdf',
     version='0.1',
     description='A simple package to visually compare PDF files',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Mor Dabastany',
-    author_email='morpci@gmail.com.com',
+    author_email='morpci@gmail.com',
     packages=find_packages(),
     install_requires=[
         'pymupdf',
