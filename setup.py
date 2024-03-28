a8d8c38886168b6c66fd1e3d887fededccbe3d99
Mor Dabastany <morpci@gmail.com>
2024-03-28 22:40:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='compare_pdf',
-    version='0.1',
+    version='0.2',
     description='A simple package to visually compare PDF files',
     long_description=long_description,
     long_description_content_type="text/markdown",
